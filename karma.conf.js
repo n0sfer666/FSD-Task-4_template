0d3c09e6d09a52de41b4da0e6d7c8bd776f191e8
Khasanov Petr <pietroismad@gmail.com>
2019-12-11 22:52:10 +0300

    add ts support to the test environment

--- karma.conf.js
+++ karma.conf.js
@@ -10,13 +10,13 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', "karma-typescript"],
 
 
     // list of files / patterns to load in the browser
     files: [
-        // './Test/*.spec.ts',
-        './Test/**/*.spec.js'        
+        './Test/**/*.spec.ts',
+        // './Test/**/*.spec.js'        
     ],
 
 
@@ -28,13 +28,26 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        "**/*.ts": "karma-typescript"
     },
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'spec'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec'],
+    reporters: ['mocha', 'karma-typescript'],
 
+    // custom config for TS
+    karmaTypescriptConfig: {
+      compilerOptions: {
+          module: "es6",
+          target: "ES5",
+          strict: true,
+          noImplicitAny: true,
+          sourceMap: true,
+          removeComments: true
+      },
+      exclude: ["node_modules"]
+  },
 
     // web server port
     port: 9876,
