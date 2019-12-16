510ba918e1028eab57d540fecec9756e6d429e4b
Khasanov Petr <pietroismad@gmail.com>
2019-12-16 18:38:05 +0300

    add: 2 tests (4 of 4) func: calculate_array_of...

--- src/Test/Model/Calculator.spec.ts
+++ src/Test/Model/Calculator.spec.ts
@@ -12,6 +12,17 @@ describe('Model -> Calculator', () => {
             let result: number[] = [-10, -7.5, -5, -2.5, -2];
             let toExpect:number[] = calculator.calculate_range_of_available_values([-10,-2], 2.5);
             expect(toExpect).toEqual(result);
-        })
-    })
+        });
+        it('param: range = [-5, 3], step = 3', () => {
+            let result: number[] = [-5, -2, 1, 3];
+            let toExpect:number[] = calculator.calculate_range_of_available_values([-5, 3], 3);
+            expect(toExpect).toEqual(result);
+        });
+        it('param: range = [-5, 3], step = 2.5', () => {
+            let result: number[] = [-5, -2.5, 0, 2.5, 3];
+            let toExpect:number[] = calculator.calculate_range_of_available_values([-5, 3], 2.5);
+            expect(toExpect).toEqual(result);
+        });
+    });
+    
 })
