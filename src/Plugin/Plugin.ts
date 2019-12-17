804da6ab0d23729b1468418b51749b625f6bf75b
Khasanov Petr <pietroismad@gmail.com>
2019-12-17 23:25:28 +0300

    git now it's template

--- src/Plugin/Plugin.ts
+++ src/Plugin/Plugin.ts
@@ -11,15 +11,6 @@ export class SimpleRangeSlider {
             tooltip: false
         }
 
-<<<<<<< HEAD
-        let model_Configuration: IModelData = {
-            orientation: this.configuration.orientation === undefined ? default_Configuration.orientation : this.configuration.orientation,
-            start: this.configuration.start === undefined ? default_Configuration.start :this. configuration.start,
-            range: this.configuration.range === undefined ? default_Configuration.range :this. configuration.range,
-            step: this.configuration.step === undefined ? default_Configuration.step :this. configuration.step,
-            connect: this.configuration.connect === undefined ? default_Configuration.connect : this.configuration.connect,
-            tooltip: this.configuration.tooltip === undefined ? default_Configuration.tooltip : this.configuration.tooltip
-=======
         let configuration: IConfiguration = {
             orientation: this.user_configuration.orientation === undefined ? default_Configuration.orientation : this.user_configuration.orientation,
             start: this.user_configuration.start === undefined ? default_Configuration.start : this.user_configuration.start,
@@ -27,7 +18,6 @@ export class SimpleRangeSlider {
             step: this.user_configuration.step === undefined ? default_Configuration.step : this.user_configuration.step,
             connect: this.user_configuration.connect === undefined ? default_Configuration.connect : this.user_configuration.connect,
             tooltip: this.user_configuration.tooltip === undefined ? default_Configuration.tooltip : this.user_configuration.tooltip
->>>>>>> iss1
         }
 
         // tmp
--- src/Plugin/Typings/Model.d.ts
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-interface IModelData extends IConfiguration {
-    size_of_Slider_in_px?: number;
-    range_of_available_values?: number[] | boolean;
-}
-=======
->>>>>>> iss1
